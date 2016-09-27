2ecf4fa76d896f87d7ca2939233396f228fd75af
student <180122@edu.p.lodz.pl>
2016-09-27 12:30:02 +0200

    main function added

--- zadanie1/czesc2.py
+++ zadanie1/czesc2.py
@@ -4,6 +4,6 @@ Created on 27 wrz 2016
 @author: student
 '''
 def second_part(data):
-    with open(data['file_name', 'w']) as f:
+    with open(data['file_name'], 'w') as f:
         f.write('Hello world!')
         print(data['poprawny'])
---
+++ zadanie1/main.py
@@ -0,0 +1,9 @@
+'''
+Created on 27 wrz 2016
+
+@author: student
+'''
+from czesc1 import first_part
+from czesc2 import second_part 
+
+second_part(first_part())
